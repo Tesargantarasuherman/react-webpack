89ee2b16f413ad493ef2a720ed270d64d6d8d260
unknown <tgantara@gmail.com>
2021-11-11 13:36:05 +0700

    first commit

---
+++ webpack-reactjs/src/App.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+import Tes from "./components/Tes";
+
+class App extends Component{
+    render(){
+        return(
+            <Tes /> 
+        )
+    }
+}
+export default App;
---
+++ webpack-reactjs/src/components/Tes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tes() {
+    return (
+        <div>
+            Component Tes
+        </div>
+    )
+}
+
+export default Tes
